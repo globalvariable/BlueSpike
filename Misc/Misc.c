32d1e2332efc626fc8485bf30f056f6b08a199d7
Mehmet Kocaturk <kocaturk@gmail.com>
2011-12-19 11:12:55 +0200

    Cosmetic fix in interrogate mapping.

--- Misc/Misc.c
+++ Misc/Misc.c
@@ -47,11 +47,11 @@ bool interrogate_mapping(void)
 		{	
 			if ((shared_memory->daq_mwa_map[i][j].mwa == MAX_NUM_OF_MWA) && (shared_memory->daq_mwa_map[i][j].channel == MAX_NUM_OF_CHAN_PER_MWA))
 			{
-				printf ("DAQ: %d   Channel: %d  ----> MWA: Idle  Channel: Idle\n", i, j);
+				printf ("DAQ: %d\tChannel: %d\t----> MWA: Idle\tChannel: Idle\n", i, j);
 			}
 			else if ((shared_memory->daq_mwa_map[i][j].mwa != MAX_NUM_OF_MWA) && (shared_memory->daq_mwa_map[i][j].channel == MAX_NUM_OF_CHAN_PER_MWA))
 			{
-				printf ("DAQ: %d   Channel: %d  ----> MWA: %d  Channel: Idle\n", i, j, shared_memory->daq_mwa_map[i][j].mwa);			
+				printf ("DAQ: %d\tChannel: %d\t----> MWA: %d\tChannel: Idle\n", i, j, shared_memory->daq_mwa_map[i][j].mwa);			
 				printf ("****************************************************************\n");			
 				printf ("*************************BUG*********************************\n");
 				printf ("BUG: MWA was not mapped but its channel was mapped\n");
@@ -61,7 +61,7 @@ bool interrogate_mapping(void)
 			}
 			else if ((shared_memory->daq_mwa_map[i][j].mwa == MAX_NUM_OF_MWA) && (shared_memory->daq_mwa_map[i][j].channel != MAX_NUM_OF_CHAN_PER_MWA))
 			{
-				printf ("DAQ: %d   Channel: %d  ----> MWA: Idle Channel: %d\n", i, j, shared_memory->daq_mwa_map[i][j].channel);			
+				printf ("DAQ: %d\tChannel: %d\t----> MWA: Idle\tChannel: %d\n", i, j, shared_memory->daq_mwa_map[i][j].channel);			
 				printf ("*******************************************************************\n");			
 				printf ("*************************BUG************************************\n");
 				printf ("BUG: MWA was mapped but its channel was not mapped\n");
@@ -71,21 +71,49 @@ bool interrogate_mapping(void)
 			}			
 			else
 			{
-				printf ("DAQ: %d   Channel: %d  ----> MWA: %d   Channel: %d\n", i, j, shared_memory->daq_mwa_map[i][j].mwa, shared_memory->daq_mwa_map[i][j].channel);
+				printf ("DAQ: %d\tChannel: %d\t----> MWA: %d\tChannel: %d\n", i, j, shared_memory->daq_mwa_map[i][j].mwa, shared_memory->daq_mwa_map[i][j].channel);
 				check =1;
+			}
+		}
+	}
+
+	printf("---------------------------------------------------------------------------------------------\n");						
+	for (i = 0; i<MAX_NUM_OF_MWA; i++)
+	{	
+		for (j = 0; j<MAX_NUM_OF_CHAN_PER_MWA; j++)
+		{	
+			if ((shared_memory->mwa_daq_map[i][j].daq_card == MAX_NUM_OF_DAQ_CARD) && (shared_memory->mwa_daq_map[i][j].daq_chan == MAX_NUM_OF_CHANNEL_PER_DAQ_CARD))
+			{
+				printf ("MWA: %d\tChannel: %d\t----> DAQ: Idle\tChannel: Idle\n", i, j);
+			}
+			else if ((shared_memory->mwa_daq_map[i][j].daq_card != MAX_NUM_OF_DAQ_CARD) && (shared_memory->mwa_daq_map[i][j].daq_chan == MAX_NUM_OF_CHANNEL_PER_DAQ_CARD))
+			{
+				printf ("DAQ: %d\tChannel: %d\t----> DAQ: %d\tChannel: Idle\n", i, j,shared_memory->mwa_daq_map[i][j].daq_card);			
+				printf ("****************************************************************\n");			
+				printf ("*************************BUG*********************************\n");
+				printf ("BUG: DAQ Card was not mapped but its channel was mapped\n");
+				printf ("*************************BUG*********************************\n");
+				printf ("****************************************************************\n");	
+				return 0;					
+			}
+			else if ((shared_memory->mwa_daq_map[i][j].daq_card == MAX_NUM_OF_DAQ_CARD) && (shared_memory->mwa_daq_map[i][j].daq_chan != MAX_NUM_OF_CHANNEL_PER_DAQ_CARD))
+			{
+				printf ("DAQ: %d\tChannel: %d\t----> DAQ: Idle\tChannel: %d\n", i, j, shared_memory->mwa_daq_map[i][j].daq_chan);			
+				printf ("*******************************************************************\n");			
+				printf ("*************************BUG************************************\n");
+				printf ("BUG: DAQ Card was mapped but its channel was not mapped\n");
+				printf ("*************************BUG************************************\n");
+				printf ("******************************************************************\n");	
+				return 0;					
+			}		
+			else
+			{
+				printf("MWA: %d\tChannel: %d\t----> DAQ: %d\tChannel: %d\n", i, j, shared_memory->mwa_daq_map[i][j].daq_card , shared_memory->mwa_daq_map[i][j].daq_chan );	
 			}
 		}
 	}
 	if (check == 1)
 	{
-		printf("---------------------------------------------------------------------------------------------\n");						
-		for (i = 0; i<MAX_NUM_OF_MWA; i++)
-		{	
-			for (j = 0; j<MAX_NUM_OF_CHAN_PER_MWA; j++)
-			{	
-				printf("MWA: %d   Channel: %d  ----> DAQ Card: %d   Channel: %d\n", i, j, shared_memory->mwa_daq_map[i][j].daq_card , shared_memory->mwa_daq_map[i][j].daq_chan );	
-			}
-		}
 		return 1;
 	}
 	else
