6595a8cf2b9902ff864058fefe1c97c21fef0cc2
Mehmet Kocaturk <kocaturk@gmail.com>
2012-01-06 16:05:36 +0200

    define MAX_TME_STAMP

--- TimeStamp.h
+++ TimeStamp.h
@@ -1,6 +1,8 @@
 #ifndef TIMESTAMP_H
 #define TIMESTAMP_H
 
+#define MAX_TIME_STAMP	0xFFFFFFFFFFFFFFFFULL		// 8 bytes
+
 typedef long long unsigned int TimeStamp;     //8 bytes makes 18 446 744 073 709 551 615 nanoseconds 5124095.57603043100417 hours
 typedef unsigned int TimeStampMs;     
 
