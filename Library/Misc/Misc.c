06adb9d4be0167b4124be982aa0b14761f918e02
Mehmet Kocaturk <kocaturk@gmail.com>
2012-01-12 00:40:30 +0200

    added is_allocated function into misc.h

--- Library/Misc/Misc.c
+++ Library/Misc/Misc.c
@@ -34,7 +34,55 @@ int get_word_in_line(char separator, int word_order, char *word, char *line, boo
 	}
 	if (print_warning)
 		printf ("%dth word couldn't be found in line\n", word_order);
+
 	return 0; // couldnt find that word
 }
 
+bool is_allocated(void* data, bool print_message, char *module, char *function, int message_type, char *message)
+{
+	if (data == NULL)
+	{
+		if (print_message)
+		{
+			if (message_type == ALLOCATION_FAILED)
+			{
+				printf("%s : %s : ERROR: Given data was not allocated\n", module, function);
+				printf("Message: %s\n", message);
+			}
+			else if (message_type == ALLOCATION_WARNING)
+			{
+				printf("%s : %s : WARNING: Given data was not allocated\n", module, function);
+				printf("Message: %s\n", message);				
+			}
+			else 
+			{
+				printf("%s : %s : BUG: Given data was not allocated\n", module, function);
+				printf("Message: %s\n", message);						
+			}
+		}
+		return FALSE;
+	}
+	else
+	{
+		if (print_message)
+		{
+			if (message_type == ALLOCATION_FAILED)
+			{
+				printf("%s : %s : ERROR: Given data was allocated\n", module, function);
+				printf("Message: %s\n", message);
+			}
+			else if (message_type == ALLOCATION_WARNING)
+			{
+				printf("%s : %s : WARNING: Given data was allocated\n", module, function);
+				printf("Message: %s\n", message);				
+			}
+			else 
+			{
+				printf("%s : %s : BUG: Given data was allocated\n", module, function);
+				printf("Message: %s\n", message);						
+			}
+		}	
+		return TRUE;
+	}
+}
 
--- Library/Misc/Misc.h
+++ Library/Misc/Misc.h
@@ -4,7 +4,13 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <gtk/gtk.h>
+
+#define ALLOCATION_FAILED 		0
+#define ALLOCATION_WARNING 	1
+#define ALLOCATION_BUG	 	2
 
 int get_word_in_line(char separator, int word_order, char *word, char *line, bool print_warning);
+bool is_allocated(void* data, bool print_message, char *module, char *function, int message_type, char *message);
 
 #endif
