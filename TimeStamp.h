9f804f53a76e7f0ef81aaa8db93262fa04a42046
Mehmet Kocaturk <kocaturk@gmail.com>
2012-01-30 22:03:45 +0200

    Removed unnecessary TimeStampMs. Use nanosecond resolution all the time.

--- TimeStamp.h
+++ TimeStamp.h
@@ -4,7 +4,7 @@
 #define MAX_TIME_STAMP	0xFFFFFFFFFFFFFFFFULL		// 8 bytes
 
 typedef long long unsigned int TimeStamp;     //8 bytes makes 18 446 744 073 709 551 615 nanoseconds 5124095.57603043100417 hours
-typedef unsigned int TimeStampMs;     
+
 
 #endif
 
