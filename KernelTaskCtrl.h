9d76f92de8ddc45a5ea4b10c3f862343c8aa687b
Mehmet Kocaturk <kocaturk@gmail.com>
2012-02-03 00:54:29 +0200

    recorder_recording status flag

--- KernelTaskCtrl.h
+++ KernelTaskCtrl.h
@@ -14,6 +14,7 @@ typedef struct __KernelTaskCtrl
 	bool		turn_daq_card_on;
 	bool		daq_card_mapped;	
 	bool		kill_all_rt_tasks;		// write here from any task to stop all rt tasks. KernelSpike will stop rt_timer. Do not stop rt_timer from any other rt_task. Freezes. . 
+	bool 		recorder_recording;          // other tasks checks this flag to start_stop recording.
 } KernelTaskCtrl;
 
 
