17d4d9b810832c068a0ff96eb21e69511589ccfa
Mehmet Kocaturk <mehmet.kocaturk@boun.edu.trmkdir>
2012-08-01 02:48:15 +0300

    Addition of rt message handler for mov obj handler to neural net simulator

--- RtTaskStatsViewer/RtTaskStatsViewer.c
+++ RtTaskStatsViewer/RtTaskStatsViewer.c
@@ -15,7 +15,7 @@ int main( int argc, char *argv[])
 
 	while (stay_alive)
 	{
-		printf("Enter command(list / clear_all / kill / task / clear_cpu / clear_cpu_thread) :\n");		
+		printf("Enter command(list / clear_all / kill / task / clear_cpu / clear_cpus_023 / clear_cpu_thread) :\n");		
 		sink = scanf("%s", arg_str);
 		if ((strcmp(arg_str, "list" ) == 0))
 		{
@@ -125,6 +125,39 @@ int main( int argc, char *argv[])
 			{
 				print_message(ERROR_MSG ,"RtTaskStatsViewer", "RtTaskStatsViewer", "main", "Inconvenient cpu num.\n");			
 			}	
+		}
+		else if  (strcmp(arg_str, "clear_cpus_023" ) == 0)
+		{
+			for (j = 0; j < MAX_NUM_OF_CPU_THREADS_PER_CPU; j++)
+			{	
+				for(k = 0; k < MAX_NUM_OF_RT_TASKS_PER_CPU_THREAD; k++)
+				{
+					rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].task_name[0] = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_task_run_time = 0;		
+		       		 	rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_positive_jitter = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_negative_jitter = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].positive_jitter_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].negative_jitter_threshold = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_positive_jitter_exceeding_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[0].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_negative_jitter_exceeding_threshold = 0;
+					rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].task_name[0] = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_task_run_time = 0;		
+		       		 	rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_positive_jitter = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_negative_jitter = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].positive_jitter_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].negative_jitter_threshold = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_positive_jitter_exceeding_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[2].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_negative_jitter_exceeding_threshold = 0;
+					rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].task_name[0] = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_task_run_time = 0;		
+		       		 	rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_positive_jitter = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].max_negative_jitter = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].positive_jitter_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].negative_jitter_threshold = 0;	
+		       		 	rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_positive_jitter_exceeding_threshold = 0;		
+			 		rt_tasks_data->cpus_rt_task_data[3].cpu_threads_rt_data[j].cpu_thread_tasks_rt_data[k].num_of_negative_jitter_exceeding_threshold = 0;
+				}			
+			}				
 		}
 		else if  (strcmp(arg_str, "clear_cpu_thread" ) == 0)
 		{			
